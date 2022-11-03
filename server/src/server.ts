bb7e875f562ba61fd1fed99d95c169654cf925c4
Romeu Castro <romeu.castro@iteriam.es>
2022-11-03 13:33:06 +0100

    Relational model created

--- server/src/server.ts
+++ server/src/server.ts
@@ -55,7 +55,7 @@ async function bootstrap() {
             }
         })
 
-        return reply.status(201).send(code)
+        return reply.status(201).send({code})
      })
 
     await fastify.listen({ port: 3333, host: '0.0.0.0' })
