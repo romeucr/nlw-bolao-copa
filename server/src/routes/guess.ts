a07d301b45286a50af8165693a4457fbee91765a
Romeu Castro <romeu.castro@iteriam.es>
2022-11-08 16:08:39 +0100

    Refactor routes

---
+++ server/src/routes/guess.ts
@@ -0,0 +1,13 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+
+export async function guessRoutes(fastify: FastifyInstance) {
+
+    /*======= GET GUESSES COUNT =======*/
+    fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count()
+             
+        return {count}
+    })
+
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from 'prisma/prisma-client'
-import { z } from 'zod'
-import ShortUniqueId from 'short-unique-id'
-
-const prisma = new PrismaClient({
-    log: ['query']
-})
+import { poolRoutes } from './routes/pool'
+import { authRoutes } from './routes/auth'
+import { gameRoutes } from './routes/game'
+import { guessRoutes } from './routes/guess'
+import { userRoutes } from './routes/user'
 
 async function bootstrap() {
     const fastify = Fastify({
@@ -17,46 +15,12 @@ async function bootstrap() {
         origin: true
     })
 
-     /*======= GET POOLS COUNT =======*/
-    fastify.get('/pools/count', async () => {
-       const count = await prisma.pool.count()
-        
-        return {count}
-    })
     
-    /*======= GET USERS COUNT =======*/
-    fastify.get('/users/count', async () => {
-        const count = await prisma.user.count()
-         
-         return {count}
-     })
-
-    /*======= GET GUESSES COUNT =======*/
-    fastify.get('/guesses/count', async () => {
-        const count = await prisma.guess.count()
-             
-        return {count}
-    })
-
-     /*======= POST POOL =======*/
-    fastify.post('/pools', async (request, reply) => {
-        const createPoolBody = z.object({
-            title: z.string(),
-        })
-
-        const { title } = createPoolBody.parse(request.body)
-
-        const generate = new ShortUniqueId({ length: 6 })
-        const  code = String(generate()).toUpperCase()
-        await prisma.pool.create({
-            data: {
-                title,
-                code
-            }
-        })
-
-        return reply.status(201).send({code})
-     })
+    await fastify.register(authRoutes)
+    await fastify.register(gameRoutes)
+    await fastify.register(guessRoutes)
+    await fastify.register(poolRoutes)
+    await fastify.register(userRoutes)
 
     await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
